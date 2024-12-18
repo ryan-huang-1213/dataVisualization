bbd2fd808b6430ade4a7905ca01952b29c191339
Haooo517 <101298971+Haooo517@users.noreply.github.com>
2024-12-18 12:33:58 +0800

    吸菸資料處理檔案小調整

--- smoke_preprocess.py
+++ smoke_preprocess.py
@@ -4,13 +4,16 @@ import pandas as pd
 file_path = './dataset/18歲以上人口目前吸菸率_utf8.csv'
 data = pd.read_csv(file_path)
 
+data.rename(columns={data.columns[0]: 'year'}, inplace=True)
+
 if '類別' in data.columns:
     data = data[data['類別'].str.contains('年齡別=|整體', na=False)]
 
 # 修改「類別」欄位，移除「年齡別=」和「歲」，並將空格改為連字號
 if '類別' in data.columns:
     data['類別'] = data['類別'].str.replace('年齡別=', '', regex=False).str.replace('歲', '', regex=False)
-    data['類別'] = data['類別'].str.replace('以上', 'up', regex=False)
+    data['類別'] = data['類別'].str.replace('以上', ' up', regex=False)
+    data['類別'] = data['類別'].str.replace('整體', 'All', regex=False)
 
 # 儲存修改後的結果到新的 CSV 文件
 output_path = './dataset/updated_smoking_rate_data.csv'
